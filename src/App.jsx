4a5f6a3e72b6aa3edde35031c04167a820ccfbb1
Harshit Pandey <harshit2001pandey@gmail.com>
2025-03-27 13:25:16 +0530

    useContext- Data passed from GrandParent

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import "./App.css";
 import {UserContext} from  "./store/provider.js"
 import Parent from "./components/Parent.jsx"
-import { use, useState } from "react";
+import { useState } from "react";
 
 function App() {
   const [user, setUser] = useState("Harshit");
