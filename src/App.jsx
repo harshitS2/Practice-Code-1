8b1830b91654dff9815dc3dcc46b8c57e0e2b08b
Harshit Pandey <harshit2001pandey@gmail.com>
2025-03-27 13:44:11 +0530

    useContext passing data and functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-
 import "./App.css";
-import {UserContext} from  "./store/provider.js"
-import Parent from "./components/Parent.jsx"
+import { UserContext } from "./store/provider.js";
+import Parent from "./components/Parent.jsx";
 import { useState } from "react";
 
 function App() {
   const [user, setUser] = useState("Harshit");
   return (
-    <UserContext.Provider value={user}>
-      <Parent /> 
+    <UserContext.Provider value={{ user, setUser }}>
+      <div className="flex items-center text-center justify-center ">
+        <Parent />
+      </div>
     </UserContext.Provider>
   );
 }
--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -1,14 +1,23 @@
-import React, { useContext } from 'react'
-import { UserContext } from '../store/provider';
+import React, { useContext, useRef } from "react";
+import { UserContext } from "../store/provider";
 
 const Child = () => {
-  const user =  useContext(UserContext);
+  const {user,setUser} = useContext(UserContext);
+  const name = useRef("");
+  const updateName = ()=>{
+    setUser(name.current.value);
+  }
   return (
     <div>
       Child is rendering user data coming from parent directly
-      {user}
+      <div>
+            <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First name</label>
+            <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter you name" ref={name} required />
+        </div>
+      <p className="text-2xl">Name is {user}</p>
+      <button type="button" className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" onClick={updateName}>Update</button>
     </div>
-  )
-}
+  );
+};
 
 export default Child;
