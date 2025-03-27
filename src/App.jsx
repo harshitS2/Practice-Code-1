5426ce2c1875d897b4c10783c5c5cd52073438dd
Harshit Pandey <harshit2001pandey@gmail.com>
2025-03-27 11:21:48 +0530

    useContext Started

--- src/App.jsx
+++ src/App.jsx
@@ -2,62 +2,9 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [data, setData] = useState([]);
 
-  useEffect(() => {
-    fetch("https://dummyjson.com/posts")
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data.posts); 
-      });
-  }, []);
 
-  return data.length === 0 ? (
-    <h1 className="text-center text-2xl mt-10">Loading...</h1>
-  ) : (
-    <div className="flex items-center justify-center">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold my-6">UseEffect</h1>
-        <p>Renders each time posts data changes</p>
-        {data.map((post) => (
-          <div
-            key={post.id}
-            className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 mb-4"
-          >
-            <a href="#">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                {post.title}
-              </h5>
-            </a>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              {post.body}
-            </p>
-            <a
-              href="#"
-              className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Read more
-              <svg
-                className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                />
-              </svg>
-            </a>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+  return <>useContext</>;
 }
 
 export default App;
