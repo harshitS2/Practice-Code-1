d07442563a5c97b770078178a521f7ca5aebe79e
Harshit Pandey <harshit2001pandey@gmail.com>
2025-03-27 11:23:10 +0530

    useContext Started

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-
-
-  return <>useContext</>;
+  return <div className="flex items-center justify-center">
+    <h1 className="font-bold text-4xl">useContext</h1>
+  </div>;
 }
 
 export default App;
