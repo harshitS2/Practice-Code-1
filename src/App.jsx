ee23700383c1c567062172044c0071a3305f349d
Harshit Pandey <harshit2001pandey@gmail.com>
2025-03-28 13:41:57 +0530

    same using useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-import "./App.css";
-import { UserContext } from "./store/provider.js";
-import Parent from "./components/Parent.jsx";
 import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [user, setUser] = useState("Harshit");
+  const [count, setCount] = useState(0);
   return (
-    <UserContext.Provider value={{ user, setUser }}>
-      <div className="flex items-center text-center justify-center ">
-        <Parent />
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h2 className="font-bold text-5xl">useReducer</h2>
+      <div className="text-center items-center">
+        <h3 className="text-xl font-semibold text-gray-800">{count}</h3>
+        <div className="flex gap-4 p-4">
+          <button
+            className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition"
+            onClick={() => setCount((prev) => prev + 1)}
+          >
+            Increase
+          </button>
+          <button
+            className="px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600 transition"
+            onClick={() => setCount((prev) => prev - 1)}
+          >
+            Decrease
+          </button>
+        </div>
       </div>
-    </UserContext.Provider>
+    </div>
   );
 }
 
